5d553e51c87099e8917482b75c05cc2d162b5a63
Zhancun Mu <91790440+namespacebilibili@users.noreply.github.com>
2023-06-04 22:12:49 +0800

    Update function.py

--- function.py
+++ function.py
@@ -140,3 +140,110 @@ def validation(args, val_dataset, net: nn.Module):
                 cur += chunk
             pbar.update()
     return total_loss / batch_num
+
+
+def train_sam(args, net: nn.Module, optimizer, train_loader,
+          epoch, writer, schedulers=None, vis = 50):
+    hard = 0
+    epoch_loss = 0
+    ind = 0
+    # train mode
+    net.train()
+    optimizer.zero_grad()
+
+    epoch_loss = 0
+    GPUdevice = torch.device('cuda:' + str(args.gpu_device))
+    device = GPUdevice
+
+    if args.thd:
+        lossfunc = DiceCELoss(sigmoid=True, squared_pred=True, reduction='mean')
+    else:
+        lossfunc = criterion_G
+
+    with tqdm(total=len(train_loader), desc=f'Epoch {epoch}', unit='img') as pbar:
+        for pack in train_loader:
+            imgs = pack['image'].to(dtype = torch.float32, device = GPUdevice)
+            masks = pack['label'].to(dtype = torch.float32, device = GPUdevice)
+            # for k,v in pack['image_meta_dict'].items():
+            #     print(k)
+            if 'pt' not in pack:
+                imgs, pt, masks = generate_click_prompt(imgs, masks)
+            else:
+                pt = pack['pt']
+                point_labels = pack['p_label']
+            name = pack['image_meta_dict']['filename_or_obj']
+
+            if args.thd:
+                pt = rearrange(pt, 'b n d -> (b d) n')
+                imgs = rearrange(imgs, 'b c h w d -> (b d) c h w ')
+                masks = rearrange(masks, 'b c h w d -> (b d) c h w ')
+
+                imgs = imgs.repeat(1,3,1,1)
+                point_labels = torch.ones(imgs.size(0))
+
+                imgs = torchvision.transforms.Resize((args.image_size,args.image_size))(imgs)
+                masks = torchvision.transforms.Resize((args.out_size,args.out_size))(masks)
+
+            showp = pt
+
+            mask_type = torch.float32
+            ind += 1
+            b_size,c,w,h = imgs.size()
+            longsize = w if w >=h else h
+
+            if point_labels[0] != -1:
+                # point_coords = samtrans.ResizeLongestSide(longsize).apply_coords(pt, (h, w))
+                point_coords = pt
+                coords_torch = torch.as_tensor(point_coords, dtype=torch.float, device=GPUdevice)
+                labels_torch = torch.as_tensor(point_labels, dtype=torch.int, device=GPUdevice)
+                coords_torch, labels_torch = coords_torch[None, :, :], labels_torch[None, :]
+                pt = (coords_torch, labels_torch)
+
+            '''init'''
+            if hard:
+                true_mask_ave = (true_mask_ave > 0.5).float()
+                #true_mask_ave = cons_tensor(true_mask_ave)
+            imgs = imgs.to(dtype = mask_type,device = GPUdevice)
+
+            '''Train'''
+            for n, value in net.image_encoder.named_parameters():
+                if "Adapter" not in n:
+                    value.requires_grad = False
+            imge= net.image_encoder(imgs)
+
+            with torch.no_grad():
+                # imge= net.image_encoder(imgs)
+                se, de = net.prompt_encoder(
+                    points=pt,
+                    boxes=None,
+                    masks=None,
+                )
+            pred, _ = net.mask_decoder(
+                image_embeddings=imge,
+                image_pe=net.prompt_encoder.get_dense_pe(), 
+                sparse_prompt_embeddings=se,
+                dense_prompt_embeddings=de, 
+                multimask_output=False,
+              )
+
+            loss = lossfunc(pred, masks)
+
+            pbar.set_postfix(**{'loss (batch)': loss.item()})
+            epoch_loss += loss.item()
+            loss.backward()
+
+            # nn.utils.clip_grad_value_(net.parameters(), 0.1)
+            optimizer.step()
+            optimizer.zero_grad()
+
+            '''vis images'''
+            if vis:
+                if ind % vis == 0:
+                    namecat = 'Train'
+                    for na in name:
+                        namecat = namecat + na.split('/')[-1].split('.')[0] + '+'
+                    vis_image(imgs,pred,masks, os.path.join(args.path_helper['sample_path'], namecat+'epoch+' +str(epoch) + '.jpg'), reverse=False, points=showp)
+
+            pbar.update()
+
+    return loss
