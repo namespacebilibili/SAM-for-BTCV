8852e08d8c2eb142e607b15c90b858f71eaee1be
namespacebilibili <muzhancun@126.com>
2023-06-18 21:20:30 +0800

    update train.py

--- train.py
+++ train.py
@@ -6,123 +6,65 @@ import torch.optim as optim
 from sklearn.metrics import roc_auc_score, accuracy_score,confusion_matrix
 import torchvision.transforms as transforms
 from tensorboardX import SummaryWriter
-#from models.discriminatorlayer import discriminator
 from conf import settings
-import time
 import cfg
-from torch.utils.data import random_split
 from utils import *
-import function 
 from dataloader import *
-import logging
+import os
+from utils import *
+from function import train_sam, validation
+import gc
+import time
+
+gc.collect()
+torch.cuda.empty_cache()
 
 args = cfg.parse_args()
-
+if not os.path.exists(f'log1/{args.exp}'):
+    os.makedirs(f'log1/{args.exp}')
+sw1 = SummaryWriter(log_dir=f'log1/{args.exp}')
 
 GPUdevice = torch.device('cuda', args.gpu_device)
+print(GPUdevice)
+net = get_network(args, args.net, use_gpu=args.gpu, gpu_device=GPUdevice, distribution=args.distributed)
 
-net = get_network(args, args.net, use_gpu=args.gpu, gpu_device=GPUdevice, distribution = args.distributed)
+if not os.path.exists(f'checkpoint/{args.exp}'):
+    os.makedirs(f'checkpoint/{args.exp}')
+nice_train_loader, nice_test_loader, transform_train, transform_val, train_list, val_list = get_decath_loader(args)
+best_loss = 1e9
 
-if args.pretrain:
-    weights = torch.load(args.pretrain)
-    net.load_state_dict(weights,strict=False)
+optimizer = torch.optim.Adam([{'params': net.mask_decoder.transformer.parameters()}, 
+                              {'params': net.mask_decoder.output_hypernetworks_mlps.parameters()},
+                              {'params': net.mask_decoder.output_upscaling.parameters()}, 
+                              {'params': net.mask_decoder.iou_prediction_head.parameters()}, 
+                              {'params': net.mask_decoder.classifier_prediction_head.parameters(), 'lr':args.class_lr}],
+                             lr=args.lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)
+net.train()
+for epoch in range(args.epoch):
+    print("Epoch:",epoch)
+    if epoch <= args.epoch_change:
+        class_weight = 0
+    else: 
+        class_weight = 1
+    total_class_loss, total_loss, mDice,class_acc = train_sam(args, nice_train_loader, net, optimizer, class_weight)
+    sw1.add_scalar('train_loss',total_loss.item(),global_step=epoch)
+    sw1.add_scalar('train_class_loss',total_class_loss.item(),global_step=epoch)
+    sw1.add_scalar('train_mDice',mDice,global_step=epoch)
+    sw1.add_scalar('class_acc',class_acc,global_step=epoch)
 
-optimizer = optim.Adam(net.parameters(), lr=args.lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
-scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5) #learning rate decay
+    print(total_loss.item())
+    if total_loss.item()<best_loss:
+        best_loss = total_loss.item()
+        torch.save(net.state_dict(),f'checkpoint/{args.exp}/{epoch}.pth')
 
-
-# '''load pretrained model'''
-# if args.weights != 0:
-#     print(f'=> resuming from {args.weights}')
-#     assert os.path.exists(args.weights)
-#     checkpoint_file = os.path.join(args.weights)
-#     assert os.path.exists(checkpoint_file)
-#     loc = 'cuda:{}'.format(args.gpu_device)
-#     checkpoint = torch.load(checkpoint_file, map_location=loc)
-#     start_epoch = checkpoint['epoch']
-#     best_tol = checkpoint['best_tol']
-    
-#     net.load_state_dict(checkpoint['state_dict'],strict=False)
-
-#     path_helper = checkpoint['path_helper']
-#     logger = create_logger(path_helper['log_path'])
-#     print(f'=> loaded checkpoint {checkpoint_file} (epoch {start_epoch})')
-
-
-'''segmentation data'''
-transform_train = transforms.Compose([
-    transforms.Resize((args.image_size,args.image_size)),
-    transforms.ToTensor(),
-])
-
-transform_train_seg = transforms.Compose([
-    transforms.Resize((args.out_size,args.out_size)),
-    transforms.ToTensor(),
-])
-
-transform_test = transforms.Compose([
-    transforms.Resize((args.image_size, args.image_size)),
-    transforms.ToTensor(),
-])
-
-transform_test_seg = transforms.Compose([
-    transforms.Resize((args.out_size,args.out_size)),
-    transforms.ToTensor(),
-])
-
-
-
-
-nice_train_loader, nice_test_loader, transform_train, transform_val, train_list, val_list =get_decath_loader(args)
-
-
-'''checkpoint path and tensorboard'''
-checkpoint_path = os.path.join(settings.CHECKPOINT_PATH, args.net, settings.TIME_NOW)
-#use tensorboard
-if not os.path.exists(settings.LOG_DIR):
-    os.mkdir(settings.LOG_DIR)
-writer = SummaryWriter(log_dir=os.path.join(
-        settings.LOG_DIR, args.net, settings.TIME_NOW))
-#create checkpoint folder to save model
-if not os.path.exists(checkpoint_path):
-    os.makedirs(checkpoint_path)
-checkpoint_path = os.path.join(checkpoint_path, '{net}-{epoch}-{type}.pth')
-
-'''begain training'''
-best_acc = 0.0
-best_tol = 1e4
-for epoch in range(settings.EPOCH):
-    if args.mod == 'sam_adpt':
-        net.train()
-        time_start = time.time()
-        loss = function.train_sam(args, net, optimizer, nice_train_loader, epoch, writer, vis = args.vis)
-        logging.info(f'Train loss: {loss}|| @ epoch {epoch}.')
-        time_end = time.time()
-        print('time_for_training ', time_end - time_start)
-
-        net.eval()
-        if epoch and epoch % args.val_freq == 0 or epoch == settings.EPOCH-1:
-            tol, (eiou, edice) = function.validation_sam(args, nice_test_loader, epoch, net, writer)
-            logging.info(f'Total score: {tol}, IOU: {eiou}, DICE: {edice} || @ epoch {epoch}.')
-
-            if args.distributed != 'none':
-                sd = net.module.state_dict()
-            else:
-                sd = net.state_dict()
-
-            if tol < best_tol:
-                best_tol = tol
-                is_best = True
-
-            #     save_checkpoint({
-            #     'epoch': epoch + 1,
-            #     'model': args.net,
-            #     'state_dict': sd,
-            #     'optimizer': optimizer.state_dict(),
-            #     'best_tol': best_tol,
-            #     'path_helper': args.path_helper,
-            # }, is_best, args.path_helper['ckpt_path'], filename="best_checkpoint")
-            else:
-                is_best = False
-
-writer.close()
+net.eval()
+test_total_loss, (iou, dice), mDice, class_acc, total_mDice = validation(args, nice_test_loader, net)
+sw1.add_scalar('validation_loss',test_total_loss)
+sw1.add_scalar('iou',iou)
+sw1.add_scalar('dice',dice)
+sw1.add_scalar('mdice',mDice)
+sw1.add_scalar('validation_class_acc',class_acc)
+print("validation total loss: ",test_total_loss,iou,dice)
+print("best training loss: ",best_loss)
+print(total_mDice)
+sw1.close()
